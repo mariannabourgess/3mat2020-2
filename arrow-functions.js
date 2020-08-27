977c4b7b8f5b6713890e3b68b46340284ec6a2ef
fcintra3 <github3@faustocintra.com.br>
2020-08-27 12:17:38 +0000

    (27/08) Término da revisão de JavaScript e atividades de fixação

--- arrow-functions.js
+++ arrow-functions.js
@@ -25,3 +25,61 @@ let potencia2 = (base, exp) => base ** exp
 
 console.log(potencia(2, 5))
 console.log(potencia2(2, 5))
+
+// Funções tradicional sem argumentos
+let msgErro = function() {
+    console.log('Erro inesperado.')
+}
+
+// Retorna um número aleatório entre 0 e 99
+let aleatorio = function() {
+    // Math.random(): retorna um número aleatório entre 0 e 1
+    // Math.floor(): "corta fora" as casas decimais de um número
+    return Math.floor(Math.random() * 100)
+}
+
+msgErro()
+console.log(aleatorio())
+
+// Arrow functions sem argumentos
+// - Os parênteses vazios são obrigatórios.
+let msgErro2 = () => console.log('Erro inesperado.')
+let aleatorio2 = () => Math.floor(Math.random() * 100)
+
+msgErro2()
+console.log(aleatorio2())
+
+// Função tradicional com 1+ argumentos
+let areaTri = function(base, altura) {
+    return base * altura / 2        // Área de um triângulo
+}
+
+// Arrow function com 1+ argumentos
+// - Os argumentos devem estar obrigatoriamente entre parênteses
+let areaTri2 = (base, altura) => base * altura / 2
+
+console.log(areaTri(4, 2.5))
+console.log(areaTri2(4, 2.5))
+
+// Função tradicional com mais de uma linha de corpo
+// 5! = 5 * 4 * 3 * 2 * 1 (120)
+let fatorial = function(n) {
+    let res = 1
+    for(let i = n; i >= 1; i--) {
+        res *= i
+    }
+    return res
+}
+
+// Arrow function com mais de uma linha de corpo
+// - As chaves que delimitam o corpo da função precisam ser mantidas
+let fatorial2 = n => {
+    let res = 1
+    for(let i = n; i >= 1; i--) {
+        res *= i
+    }
+    return res
+}
+
+console.log(fatorial(5))
+console.log(fatorial2(5))
