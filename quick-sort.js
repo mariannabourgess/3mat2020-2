9ebfaae43be6cf7cc52b5ba7be601d2ddd487a5d
fcintra3 <github3@faustocintra.com.br>
2020-10-08 12:24:32 +0000

    (08/10) Término do algoritmo Quick Sort, com função de comparação

--- quick-sort.js
+++ quick-sort.js
@@ -1,19 +1,41 @@
-function quickSort(vetor) {
-    let inicio = 0
-    let fim = vetor.length - 2 // Penúltima posição
-    let posPivot = vetor.length - 1 // Última posição
-    let posDiv = inicio - 1
-    for(let i = inicio; i <= fim; i++) {
-        if(vetor[i] < vetor[posPivot]) {
-            posDiv++
-            [vetor[i], vetor[posDiv]] = [vetor[posDiv], vetor[i]]
+let passadas = 0, comparacoes = 0, trocas = 0
+function quickSort(vetor, inicio = 0, fim = vetor.length - 1) {
+    if(fim > inicio) {      // Garante pelo menos 2 elementos na ordenação
+        passadas++
+        let posPivot = fim // Última posição
+        let posDiv = inicio - 1
+        for(let i = inicio; i < fim; i++) {
+            comparacoes++
+            if(vetor[i] < vetor[posPivot]) {
+                posDiv++
+                [vetor[i], vetor[posDiv]] = [vetor[posDiv], vetor[i]]
+                trocas++
+            }
         }
+        
+        posDiv++ // Incremento final para encontrar a posição correta do pivô
+        // Troca entre a posição da divisória e a posição do vetor
+        [vetor[posDiv], vetor[posPivot]] = [vetor[posPivot], vetor[posDiv]]
+        trocas++
+
+        // Chamadas recursivas ao quickSort
+        quickSort(vetor, inicio, posDiv - 1)    // Lado esquerdo
+        quickSort(vetor, posDiv + 1, fim)       // Lado direito
     }
-    posDiv++ // Incremento final para encontrar a posição correta do pivô
-    // Troca entre a posição da divisória e a posição do vetor
-    [vetor[posDiv], vetor[posPivot]] = [vetor[posPivot], vetor[posDiv]]
 }
 
-let nums = [7, 3, 0, 8, 6, 2, 4, 9, 5, 1]
+/*
+let nums = [7, 4, 0, 8, 3, 2, 1, 9, 5, 6]
 quickSort(nums)
 console.log(nums)
+console.log({passadas, comparacoes, trocas})
+*/
+
+let nomes = require('./dados/100-mil-nomes')
+console.time('Teste nomes')
+quickSort(nomes)
+console.timeEnd('Teste nomes')
+let memoria = process.memoryUsage().heapUsed / 1024 / 1024
+console.log(nomes)
+console.log('Memória usada (MB):', memoria)
+console.log({passadas, comparacoes, trocas})
